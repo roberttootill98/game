2aabba73d24b2cdf7f6e7653474a538b5540f0ca
Rob T <Robert.Tootill98@gmail.com>
2020-04-08 12:51:02 +0100

    get cards func

---
+++ server/cards.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const cards = [
+  {
+    'name': 'bruh',
+    'attack': 1,
+    'defense': 2
+  },
+  {
+    'name': 'daniel',
+    'attack': 69,
+    'defense': 2
+  }
+]
+
+function getCards() {
+  return cards;
+}
+
+module.exports = {
+  getCards: getCards
+}
